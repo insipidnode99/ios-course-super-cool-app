09c26a25770f16a7dc45f55fc4b097d2707ad62f
e-dawg <e-dawg@e-dawgs-Mac-mini.local>
2016-01-20 01:04:49 -0500

    Initial Commit

---
+++ SuperCool - 1st app (2)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool - 1st app (2)
+//
+//  Created by e-dawg on 1/20/16.
+//  Copyright (c) 2016 iLikeiPads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
